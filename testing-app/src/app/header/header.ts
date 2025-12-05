7e028ae1e7acd7165feaa1539cbd3390d570ac8d
KS <180429671+ks9927@users.noreply.github.com>
2025-12-05 15:26:55 -0500

    created about comp and added to the page

--- testing-app/src/app/header/header.ts
+++ testing-app/src/app/header/header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { HomeComp } from '../home-comp/home-comp';
+import { AboutComp } from '../about-comp/about-comp';
 @Component({
   selector: 'app-header',
-  imports: [HomeComp],
+  imports: [HomeComp, AboutComp],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
