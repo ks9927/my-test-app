64bbb38ba7026066915fe78f5b6f7d6a4adfb513
KS <180429671+ks9927@users.noreply.github.com>
2025-12-05 15:41:05 -0500

    removed comps from headers and put basic uls instead

--- testing-app/src/app/header/header.ts
+++ testing-app/src/app/header/header.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { HomeComp } from '../home-comp/home-comp';
-import { AboutComp } from '../about-comp/about-comp';
-import { ContactComp } from '../contact-comp/contact-comp';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [HomeComp, AboutComp, ContactComp],
+  imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
